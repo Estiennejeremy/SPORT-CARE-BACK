8fd50a28c014cbfda7023ebda35ce3eecbf8021e
Jeremy <jeremy.butin@yahoo.com>
2021-04-09 15:22:39 +0200

    db users maj + package.json

--- src/models/users.js
+++ src/models/users.js
@@ -1,1 +1,42 @@
 const mongoose = require ('mongoose')
+
+const userSchema = new mongoose.Schema ({
+    Last_name: {
+        type: String,
+        require
+    },
+    First_name: {
+        type: String,
+        require
+    },
+    Birthdate: {
+        type: Date,
+        required: true
+    },
+    Adress: {
+        type: String,
+        required: true
+    },
+    Role: {
+        type: Integer,
+        required: true
+    },
+    UserDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    Civility: {
+        type: Boolean,
+        required: true
+    },
+    Mail: {
+        type: String,
+        required: true
+    },
+    Password: {
+        type: String,
+        required: true
+    }
+
+})
