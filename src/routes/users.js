db80c2c839b2b10f2ff8b53a14435c8319dfac3b
marceau-cr <marceau.cristianini@epitech.eu>
2021-04-15 18:15:51 +0200

    Merge branch 'main' into Set-up-mongodb

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,15 @@
 const express = require ('express')
 const router = express.Router()
+const Users = require('../models/users')
 
 //Getting all
 router.get ('/', (req, res) => {
-
+  try {
+    const users = await Users.find()
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
 //Getting One
