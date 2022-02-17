02efe1fc1b9a7d1878a4b3879e39ba253c7a8ab6
dash <jeremy.estienne@epitech.eu>
2022-02-17 15:51:28 +0100

    [refacto] cardiacRecords

---
+++ src/controllers/cardiacRecords.controller.js
@@ -0,0 +1,56 @@
+const cardiacModel = require("../models/cardiacRecords.model");
+const reportModel = require("../models/dailyReports.model");
+const IaClient = require("../services/ia-client");
+
+var cardiacRecordsController = {
+    index: async function (req, res) {
+        return cardiacModel.find().exec();
+    },
+    show: async function (req, res) {
+        return cardiacModel.findById(req.params.id).exec();
+    },
+    delete: async function (req, res) {
+        return cardiacModel.findByIdAndRemove(req.params.id).exec();
+    },
+    create: async function (req, res) {
+        const allRm = []
+        let status = null;
+        const allModels = reportModel.find();
+        const fiveLastRmssd = (await allModels).filter(model => model.rmssd).slice(Math.max(allModels.length - 5, 0))
+        console.log(fiveLastRmssd)
+        rmssd = await IaClient.getRmssd(req.body.hrData);
+        if (fiveLastRmssd.length >= 4) {
+
+            for (const item of fiveLastRmssd) {
+                allRm.push(item.rmssd);
+            }
+            allRm.push(rmssd)
+            status = await IaClient.getState(allRm)
+        }
+        const average = req.body.hrData.reduce((a, b) => a + b) / req.body.hrData.length;
+        const cardiacRecord = new cardiacModel({
+            dailyReportId: req.body.dailyReportId,
+            rmssd: rmssd,
+            heartRate: average,
+            hrData: req.body.hrData,
+            currentStatus: status
+        });
+        return cardiacRecord.save();
+    },
+    update: async function (req, res) {
+        return cardiacModel.findByIdAndUpdate(req.params.id, {
+            dailyReportId: req.body.dailyReportId,
+            rmssd: req.body.rmssd,
+            heartRate: req.body.heartRate,
+            hrData: req.body.hrData,
+            currentStatus: req.body.status
+        }, { new: true }).exec();
+    },
+    getReportCardiacRecords: async function (req, res, next) {
+        return cardiacModel.find({
+            userId: req.params.dailyReportId,
+        });
+    }
+};
+
+module.exports = cardiacRecordsController;
---
+++ src/routes/cardiacRecords.routes.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const cardiacModel = require("../models/cardiacRecords.model");
+const IaClient = require("../services/ia-client");
+const reportModel = require("../models/dailyReports.model");
+const authentication = require("../middlewares/authentication");
+const cardiacRecordsController = require("../controllers/cardiacRecords.controller");
+
+//Getting all
+router.get("/", authentication.checkTokenMiddleware, async (req, res) => {
+  cardiacRecordsController.index(req, res).then((cardiacRecords) => {
+    res.json(cardiacRecords);
+  }).catch((err) => {
+    console.log(err.stack);
+    res.status(500).json({ message: err.message });
+  });
+});
+
+//Getting One
+router.get(
+  "/:id",
+  [authentication.checkTokenMiddleware],
+  async (req, res) => {
+    cardiacRecordsController.show(req, res).then((cardiacRecord) => {
+      res.json(cardiacRecord);
+    }).catch((err) => {
+      console.log(err.stack);
+
+      res.status(500).json({ message: err.message });
+    });
+  }
+);
+
+//Getting all for a report
+router.get(
+  "/dailyReport/:id",
+  [authentication.checkTokenMiddleware],
+  (req, res) => {
+    cardiacRecordsController.getReportCardiacRecords(req, res).then((cardiacRecords) => {
+      res.json(cardiacRecords);
+    }).catch((err) => {
+      console.log(err.stack);
+      res.status(500).json({ message: err.message });
+    });
+  }
+);
+
+// Creating one
+router.post("/", authentication.checkTokenMiddleware, async (req, res) => {
+  cardiacRecordsController.create(req, res).then((cardiacRecord) => {
+    res.json(cardiacRecord);
+  }).catch((err) => {
+    console.log(err.stack);
+    res.status(400).json({ message: err.message });
+  });
+});
+
+//Updating One
+router.patch(
+  "/:id",
+  [authentication.checkTokenMiddleware],
+  async (req, res) => {
+    cardiacRecordsController.update(req, res).then((cardiacRecord) => {
+      res.json(cardiacRecord);
+    }).catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
+  }
+);
+
+//Deleting One
+router.delete(
+  "/:id",
+  [authentication.checkTokenMiddleware],
+  async (req, res) => {
+    cardiacRecordsController.delete(req, res).then((cardiacRecord) => {
+      res.json(cardiacRecord);
+    }).catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
+  }
+);
+
+
+
+module.exports = router;
