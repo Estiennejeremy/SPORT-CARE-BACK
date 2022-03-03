a0af84c8cd8839da4a0b1af777cd10a49e2f9c0e
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2022-03-03 13:54:29 +0100

    feat: fix userId params

--- src/controllers/trainings.controller.js
+++ src/controllers/trainings.controller.js
@@ -1,42 +1,50 @@
 const trainingModel = require("../models/trainings.model");
 
 var trainingsController = {
-    index: function (req, res) {
-        return trainingModel.find().exec();
-    },
-    show: function (req, res) {
-        return trainingModel.findById(req.params.id).exec();
-    },
-    create: function (req, res) {
-        const training = new trainingModel({
-            userId: req.body.userId,
-            date: req.body.date,
-            effort: req.body.effort,
-            recap: req.body.recap,
-            duration: req.body.duration,
-            sportID: req.body.sportID,
-        });
-        return training.save();
-    },
-    update: function (req, res) {
-        return trainingModel.findByIdAndUpdate(req.params.id, {
-            userId: req.body.userId,
-            date: req.body.date,
-            effort: req.body.effort,
-            recap: req.body.recap,
-            duration: req.body.duration,
-            sportID: req.body.sportID,
-        }, { new: true }).exec();
-    },
-    delete: function (req, res) {
-        return trainingModel.findByIdAndRemove(req.params.id).exec();
-    },
-    getUserTrainings: function (req, res) {
-        return trainingModel.find({
-            "userId": {
-                "$eq": req.params.userId
-            }
-        }).exec();
-    }
-}
+  index: function (req, res) {
+    return trainingModel.find().exec();
+  },
+  show: function (req, res) {
+    return trainingModel.findById(req.params.id).exec();
+  },
+  create: function (req, res) {
+    const training = new trainingModel({
+      userId: req.body.userId,
+      date: req.body.date,
+      effort: req.body.effort,
+      recap: req.body.recap,
+      duration: req.body.duration,
+      sportID: req.body.sportID,
+    });
+    return training.save();
+  },
+  update: function (req, res) {
+    return trainingModel
+      .findByIdAndUpdate(
+        req.params.id,
+        {
+          userId: req.body.userId,
+          date: req.body.date,
+          effort: req.body.effort,
+          recap: req.body.recap,
+          duration: req.body.duration,
+          sportID: req.body.sportID,
+        },
+        { new: true }
+      )
+      .exec();
+  },
+  delete: function (req, res) {
+    return trainingModel.findByIdAndRemove(req.params.id).exec();
+  },
+  getUserTrainings: function (req, res) {
+    return trainingModel
+      .find({
+        userId: {
+          $eq: req.params.userId,
+        },
+      })
+      .exec();
+  },
+};
 module.exports = trainingsController;
--- src/routes/trainings.routes.js
+++ src/routes/trainings.routes.js
@@ -6,49 +6,57 @@ const trainingController = require("../controllers/trainings.controller");
 
 //Getting all
 router.get("/", [authentication.checkTokenMiddleware], async (req, res) => {
-  trainingController.index(req, res).then(result => {
-    res.json(result);
-  }).catch(err => {
-    console.log(err.stack);
-    res.status(500).json({ message: err.message });
-  })
+  trainingController
+    .index(req, res)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err.stack);
+      res.status(500).json({ message: err.message });
+    });
 });
 
 //Getting One
-router.get(
-  "/:id",
-  [authentication.checkTokenMiddleware],
-  (req, res) => {
-    trainingController.show(req, res).then(result => {
+router.get("/:id", [authentication.checkTokenMiddleware], (req, res) => {
+  trainingController
+    .show(req, res)
+    .then((result) => {
       res.json(result);
-    }).catch(err => {
+    })
+    .catch((err) => {
       console.log(err.stack);
       res.status(500).json({ message: err.message });
-    })
-  }
-);
+    });
+});
 
 router.get(
-  "/user/:id",
+  "/user/:userId",
   [authentication.checkTokenMiddleware],
   (req, res) => {
-    trainingController.getUserTrainings(req, res).then(result => {
-      res.json(result);
-    }).catch(err => {
-      console.log(err.stack);
-      res.status(500).json({ message: err.message });
-    })
+    trainingController
+      .getUserTrainings(req, res)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(500).json({ message: err.message });
+      });
   }
 );
 
 // Creating one
 router.post("/", [authentication.checkTokenMiddleware], async (req, res) => {
-  trainingController.create(req, res).then(result => {
-    res.json(result);
-  }).catch(err => {
-    console.log(err.stack);
-    res.status(500).json({ message: err.message });
-  })
+  trainingController
+    .create(req, res)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err.stack);
+      res.status(500).json({ message: err.message });
+    });
 });
 
 //Updating One
@@ -56,12 +64,15 @@ router.patch(
   "/:id",
   [authentication.checkTokenMiddleware],
   async (req, res) => {
-    trainingController.update(req, res).then(result => {
-      res.json(result);
-    }).catch(err => {
-      console.log(err.stack);
-      res.status(500).json({ message: err.message });
-    });
+    trainingController
+      .update(req, res)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(500).json({ message: err.message });
+      });
   }
 );
 
@@ -70,17 +81,16 @@ router.delete(
   "/:id",
   [authentication.checkTokenMiddleware],
   async (req, res) => {
-    trainingController.delete(req, res).then(result => {
-      res.json(result);
-    }).catch(err => {
-      console.log(err.stack);
-      res.status(500).json({ message: err.message });
-    });
+    trainingController
+      .delete(req, res)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(500).json({ message: err.message });
+      });
   }
 );
-
-
-
-
 
 module.exports = router;
