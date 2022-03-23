bd906766083845dbf2a94b713a1064d6e3e38042
Lionel Bonzoumet <lionel.bonzoumet@gmail.com>
2022-03-23 19:55:20 +0100

    fix: merge conflict

--- src/controllers/dailyReports.controller.js
+++ src/controllers/dailyReports.controller.js
@@ -1,98 +1,194 @@
 const reportModel = require("../models/dailyReports.model");
+const { Double } = require("mongodb");
 
 var dailyReportsController = {
-    index: function (req, res) {
-        return reportModel.find().populate('cardiacRecordId').exec();
-    },
-    show: function (req, res) {
-        return reportModel.findById(req.params.id).populate('dailyRecords').populate('cardiacRecordId').exec();
-    },
-    create: function (req, res) {
-        const report = new reportModel({
-            userId: req.body.userId,
-            cardiacRecordId: req.body.cardiacRecordId,
-            dailyFeelings: req.body.dailyFeelings,
-            size: req.body.size,
-            date: Date.now(),
-            weight: req.body.weight,
-            rmssd: req.body.rmssd,
-            mhr: req.body.mhr,
-            bmi: req.body.bmi,
-        });
-        return report.save();
-    },
-    update: function (req, res) {
-        return reportModel.findByIdAndUpdate(req.params.id, {
-            cardiacRecordId: req.body.cardiacRecordId,
-            userId: req.body.userId,
-            dailyFeelings: req.body.dailyFeelings,
-            size: req.body.size,
-            weight: req.body.weight,
-            rmssd: req.body.rmssd,
-            mhr: req.body.mhr,
-            bmi: req.body.bmi,
-        }, { new: true }).exec();
-    },
-    delete: function (req, res) {
-        return reportModel.findByIdAndRemove(req.params.id).exec();
-    },
-    getReportWithDate: function (req, res) {
-        var d = new Date();
-        d.setHours(0);
-        d.setMinutes(0);
-        d.setSeconds(0);
+  index: function (req, res) {
+    return reportModel.find().populate("cardiacRecordId").exec();
+  },
+  show: function (req, res) {
+    return reportModel
+      .findById(req.params.id)
+      .populate("dailyRecords")
+      .populate("cardiacRecordId")
+      .exec();
+  },
+  create: function (req, res) {
+    const report = new reportModel({
+      userId: req.body.userId,
+      cardiacRecordId: req.body.cardiacRecordId,
+      dailyFeelings: req.body.dailyFeelings,
+      size: req.body.size,
+      date: Date.now(),
+      weight: req.body.weight,
+      rmssd: req.body.rmssd,
+      mhr: req.body.mhr,
+      bmi: req.body.bmi,
+    });
+    return report.save();
+  },
+  update: function (req, res) {
+    return reportModel
+      .findByIdAndUpdate(
+        req.params.id,
+        {
+          cardiacRecordId: req.body.cardiacRecordId,
+          userId: req.body.userId,
+          dailyFeelings: req.body.dailyFeelings,
+          size: req.body.size,
+          weight: req.body.weight,
+          rmssd: req.body.rmssd,
+          mhr: req.body.mhr,
+          bmi: req.body.bmi,
+        },
+        { new: true }
+      )
+      .exec();
+  },
+  delete: function (req, res) {
+    return reportModel.findByIdAndRemove(req.params.id).exec();
+  },
+  getReportWithDate: function (req, res) {
+    var d = new Date();
+    d.setHours(0);
+    d.setMinutes(0);
+    d.setSeconds(0);
 
-        return reportModel.find({
-            "userId": {
-                "$eq": req.params.userId
-            },
-            "date": {
-                "$gt": d
-            }
-        }).populate('dailyRecords').exec();
+    return reportModel
+      .find({
+        userId: {
+          $eq: req.params.userId,
+        },
+        date: {
+          $gt: d,
+        },
+      })
+      .populate("dailyRecords")
+      .exec();
+  },
+  getUserReports: function (req, res) {
+    return reportModel
+      .find({
+        userId: {
+          $eq: req.params.userId,
+        },
+      })
+      .populate("cardiacRecordId")
+      .exec();
+  },
+  getReportLast5Days: async function (req, res) {
+    let reports;
 
+    var dmin = new Date();
+    dmin.setDate(dmin.getDate() - 6);
+    dmin.setHours(0);
+    dmin.setMinutes(0);
+    dmin.setSeconds(0);
 
+    var dmax = new Date().setDate(dmin.getDate() + 5);
 
-    },
-    getUserReports: function (req, res) {
-        return reportModel.find({
-            "userId": {
-                "$eq": req.params.userId
-            }
-        }).populate('cardiacRecordId').exec();
-    },
-    getReportLast5Days: async function (req, res) {
-        let reports
-
-
-        var dmin = new Date();
-        dmin.setDate(dmin.getDate() - 6);
-        dmin.setHours(0);
-        dmin.setMinutes(0);
-        dmin.setSeconds(0);
-
-        var dmax = new Date().setDate(dmin.getDate() + 5);
-
-        reports = await reportModel.find({
-            "userId": {
-                "$eq": req.params.userId
-            },
-            "date": {
-                "$gt": dmin,
-                "$lt": dmax
-            }
-        }).populate('cardiacRecordId').exec();
-        if (reports.length > 0) {
-
-            return reports.sort((a, b) => a.date - b.date);
-        }
-        return null
-    },
-    getUserLastReport: async function (req, res) {
-        reports = await reportModel.find({ userId: req.params.userId }).populate('cardiacRecordId').exec();
-        if (reports.length > 0) {
-            return reports[reports.length - 1];
-        }
+    reports = await reportModel
+      .find({
+        userId: {
+          $eq: req.params.userId,
+        },
+        date: {
+          $gt: dmin,
+          $lt: dmax,
+        },
+      })
+      .populate("cardiacRecordId")
+      .exec();
+    if (reports.length > 0) {
+      return reports.sort((a, b) => a.date - b.date);
     }
-}
+    return null;
+  },
+  getUserLastReport: async function (req, res) {
+    reports = await reportModel
+      .find({ userId: req.params.userId })
+      .populate("cardiacRecordId")
+      .exec();
+    if (reports.length > 0) {
+      return reports[reports.length - 1];
+    }
+  },
+  getStatsonTimeSpan: async function (req, res) {
+    let _reports;
+    let _data = {
+      IMC: Number,
+      FCM: Number,
+      RMSSD: Double,
+      date: Date,
+    };
+    let _datas = [];
+    var currentDate = new Date();
+    let dmin = new Date();
+    let dmax = new Date();
+
+    try {
+      switch (req.body.timeSpan) {
+        case "week":
+          dmin.setDate(currentDate.getDate() - 7 - (currentDate.getDay() - 1));
+          dmin.setHours(0);
+          dmin.setMinutes(0);
+          dmin.setSeconds(0);
+          console.log("dmin", dmin);
+
+          _reports = await reportModel.find({
+            userId: {
+              $eq: req.body.userId,
+            },
+            date: {
+              $gt: dmin,
+              $lt: dmin.setDate(dmin.getDate() + 7),
+            },
+          });
+          break;
+        case "month":
+          dmin = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+          dmax = new Date(
+            currentDate.getFullYear(),
+            currentDate.getMonth() + 1,
+            0
+          );
+          console.log("dmin", dmin);
+          console.log("dmax", dmax);
+
+          _reports = await reportModel.find({
+            userId: {
+              $eq: req.body.userId,
+            },
+            date: {
+              $gt: dmin,
+              $lt: dmax,
+            },
+          });
+          break;
+      }
+      console.log("_reports", _reports);
+      if (_reports == null) {
+        return res.status(404).json({ message: "Cannot find report" });
+      } else {
+        _reports.forEach((report) => {
+          console.log("report", report);
+          _data = {
+            IMC: (
+              report.weight /
+              ((report.size / 100) * (report.size / 100))
+            ).toFixed(0),
+            FCM: report.mhr,
+            RMSSD: report.rmssd,
+            date: report.date.getDate(),
+          };
+          console.log("_data", _data);
+          _datas.push(_data);
+        });
+      }
+      console.log("_datas", _datas);
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
+    return _datas;
+  },
+};
 module.exports = dailyReportsController;
--- src/routes/dailyReports.routes.js
+++ src/routes/dailyReports.routes.js
@@ -6,12 +6,15 @@ const dailyReportsController = require("../controllers/dailyReports.controller")
 
 //Getting all
 router.get("/", authentication.checkTokenMiddleware, async (req, res) => {
-  dailyReportsController.index(req, res).then(result => {
-    res.json(result);
-  }).catch(err => {
-    console.log(err.stack);
-    res.status(500).json({ message: err.message });
-  })
+  dailyReportsController
+    .index(req, res)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err.stack);
+      res.status(500).json({ message: err.message });
+    });
 });
 
 //Getting all for a user
@@ -19,70 +22,106 @@ router.get(
   "/user/:userId",
   [authentication.checkTokenMiddleware],
   (req, res) => {
-    dailyReportsController.getUserReports(req, res).then(result => {
-      res.json(result);
-    }).catch(err => {
-      console.log(err.stack);
-      res.status(500).json({ message: err.message });
-    });
+    dailyReportsController
+      .getUserReports(req, res)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(500).json({ message: err.message });
+      });
   }
 );
 
 //Getting One
-router.get(
-  "/:id",
-  [authentication.checkTokenMiddleware],
-  (req, res) => {
-    dailyReportsController.show(req, res).then(result => {
+router.get("/:id", [authentication.checkTokenMiddleware], (req, res) => {
+  dailyReportsController
+    .show(req, res)
+    .then((result) => {
       res.json(result);
-    }).catch(err => {
+    })
+    .catch((err) => {
       console.log(err.stack);
       res.status(500).json({ message: err.message });
-    })
+    });
+});
+
+//Get one with date
+router.get(
+  "/date/:userId",
+  [authentication.checkTokenMiddleware],
+  (req, res) => {
+    dailyReportsController
+      .getReportWithDate(req, res)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(500).json({ message: err.message });
+      });
   }
 );
 
-//Get one with date
-router.get("/date/:userId", [authentication.checkTokenMiddleware], (req, res) => {
-  dailyReportsController.getReportWithDate(req, res).then(result => {
-    res.json(result);
-  }).catch(err => {
-    console.log(err.stack);
-    res.status(500).json({ message: err.message });
-  })
-});
-
 //getReportLast5Days
 router.get("/dateMinus5/:userId", (req, res) => {
-  dailyReportsController.getReportLast5Days(req, res).then(result => {
-    res.json(result);
-  }).catch(err => {
-    console.log(err.stack);
-    res.status(500).json({ message: err.message });
-  });
+  dailyReportsController
+    .getReportLast5Days(req, res)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err.stack);
+      res.status(500).json({ message: err.message });
+    });
 });
 
 // Creating one
 router.post("/", authentication.checkTokenMiddleware, async (req, res) => {
-  dailyReportsController.create(req, res).then(result => {
-    res.json(result);
-  }).catch(err => {
-    console.log(err.stack);
-    res.status(400).json({ message: err.message });
-  })
+  dailyReportsController
+    .create(req, res)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err.stack);
+      res.status(400).json({ message: err.message });
+    });
 });
+
+//getIMConTimeSpan
+router.post(
+  "/stats",
+  [authentication.checkTokenMiddleware],
+  async (req, res) => {
+    dailyReportsController
+      .getStatsonTimeSpan(req, res)
+      .then((result) => {
+        console.log("FUCKING RESULT", result);
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(500).json({ message: err.message });
+      });
+  }
+);
 
 //Updating One
 router.patch(
   "/:id",
   [authentication.checkTokenMiddleware],
   async (req, res) => {
-    dailyReportsController.update(req, res).then(result => {
-      res.json(result);
-    }).catch(err => {
-      console.log(err.stack);
-      res.status(400).json({ message: err.message });
-    })
+    dailyReportsController
+      .update(req, res)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(400).json({ message: err.message });
+      });
   }
 );
 
@@ -91,24 +130,29 @@ router.delete(
   "/:id",
   [authentication.checkTokenMiddleware],
   async (req, res) => {
-    dailyReportsController.delete(req, res).then(result => {
-      res.json(result);
-    }).catch(err => {
-      console.log(err.stack);
-      res.status(400).json({ message: err.message });
-    })
+    dailyReportsController
+      .delete(req, res)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err.stack);
+        res.status(400).json({ message: err.message });
+      });
   }
 );
 
 //Getting last report for a user
 router.get("/last/:userId", (req, res) => {
-  dailyReportsController.getUserLastReport(req, res).then(result => {
-    res.json(result);
-  }).catch(err => {
-    console.log(err.stack);
-    res.status(500).json({ message: err.message });
-  })
+  dailyReportsController
+    .getUserLastReport(req, res)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err.stack);
+      res.status(500).json({ message: err.message });
+    });
 });
-
 
 module.exports = router;
